fe6d1d1362a257331c4b0fe55c8e406270527020
tungnt93 <tungnt93@gmail.com>
2017-11-21 11:17:32 +0700

    rest api

--- application/controllers/api/Index.php
+++ application/controllers/api/Index.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: longnd
- * Date: 11/21/2017
- * Time: 12:16 AM
- */
+require(APPPATH.'libraries/REST_Controller.php');
+
+class Index extends REST_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->model('product_model');
+    }
+
+    function product_get(){
+        $data = $this->product_model->get_list();
+//        $this->response(NULL, 400);
+        $this->response(array('status'=>'success', 'data'=>$data), 200);
+    }
+
+}
